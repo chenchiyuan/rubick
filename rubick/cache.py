69425234d9832d58add8bf5fb6e2746066ce8718
chenchiyuan <chenchiyuan03@gmail.com>
2013-05-24 13:18:12 +0800

    如果cache挂了，他就挂了，访问会被拒绝。

--- rubick/cache.py
+++ rubick/cache.py
@@ -4,12 +4,16 @@
 from __future__ import division, unicode_literals, print_function
 import random
 failure_rate = 100 # 1/100
+AVAILABLE_FIELD = "__AVAILABLE"
 
 def problem_decorator(func):
     def wrapper(*args, **kwargs):
+        instance = args[0]
+        if not getattr(instance, AVAILABLE_FIELD, True):
+            raise Exception()
         worked = random.randint(0, failure_rate)
         if not worked:
-            raise Exception()
+            setattr(instance, AVAILABLE_FIELD, False)
         else:
             return func(*args, **kwargs)
     return wrapper
@@ -19,7 +23,8 @@ class ProblemMetaClass(type):
         for attr, item in class_dict.items():
             if callable(item):
                 class_dict[attr] = problem_decorator(item)
-
+        if not AVAILABLE_FIELD in class_dict:
+            class_dict[AVAILABLE_FIELD] = True
         return type.__new__(cls, class_name, bases, class_dict)
 
 class BaseCache(object):
